1491430fdf0cf814fdff8f5e53e324e99473b1c6
gayekayman <gayekayman@gmail.com>
2024-10-24 14:18:37 +0300

    SQL Bağlantı Yapıldı.

---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabaseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** C# Veri Tabanli Urun - Kategori  Bilgi sistemi *****");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Urunler");
+            Console.WriteLine("3-siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.Write("lutfen getirmek istediğiniz tablo numarasını giriniz: ");
+
+            string tableNumber = Console.ReadLine();
+            Console.WriteLine("------------------------");
+
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-LFJAJU5;" +
+                "initial Catalog=EgitimKampiDb;integrated security=true ");
+            connection.Open();
+            SqlCommand command = new SqlCommand("select * from TblCategory", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();//verileri geçici rem belleğe almayı sağlar.
+            adapter.Fill(dataTable);
+            connection.Close();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
